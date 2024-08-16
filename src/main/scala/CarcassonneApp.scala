a2bec63a29d5f18c08748c19180ac922bc63ec46
FPirazz <federicopirazzoli99@gmail.com>
2024-08-16 16:13:43 +0200

    feat: Implemented basic main class to test basic map functionality

---
+++ src/main/scala/CarcassonneApp.scala
@@ -0,0 +1,15 @@
+import scalafx.application.*
+import scalafx.geometry.Insets
+import scalafx.scene.Scene
+
+object CarcassonneApp extends JFXApp3:
+  override def start(): Unit =
+    val model = new GameMap()
+    val view = new ObserverGameMapView()
+    val controller = new GameMapController(model, view)
+    controller.initialize()
+
+    stage = new JFXApp3.PrimaryStage:
+      title = "Carcassonne Map"
+      scene = new Scene(600, 600):
+        root = view
