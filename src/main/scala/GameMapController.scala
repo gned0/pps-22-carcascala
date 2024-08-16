e5305e8426769a33ca198fea219472045c5d8720
FPirazz <federicopirazzoli99@gmail.com>
2024-08-16 16:12:58 +0200

    feat: Implemented Controller class with observer features to modify the Model part

---
+++ src/main/scala/GameMapController.scala
@@ -0,0 +1,21 @@
+
+class GameMapController(model: GameMap, view: ObserverGameMapView) extends Observer[GameMapView]:
+
+  def initialize(): Unit =
+  // Set up the view to listen for tile click events and place a tile
+  view.addObserver(this)
+
+  override def receiveUpdate(subject: GameMapView): Unit =
+    placeTile(subject.lastTilePlacedPosition)
+
+  def placeTile(position: Position): Unit =
+  // Here, we place a tile in the model when a position is clicked in the view
+    try
+      val tile = GameTile.startTile // Create a new tile
+      model.placeTile(tile, position)
+      log(s"Placed tile at $position")
+    catch
+      case e: IllegalArgumentException => println(e.getMessage)
+
+  def log(string: String): Unit =
+    print(s"CONTROLLER - " + string + "\n")
