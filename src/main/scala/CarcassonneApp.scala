571731d38d81afca617ffa9d9b437b3e57b85d73
FPirazz <federicopirazzoli99@gmail.com>
2024-08-17 14:56:54 +0200

    feat: Replace the temporary buttons with proper squared regions to represent the tile placeholder locations

--- src/main/scala/CarcassonneApp.scala
+++ src/main/scala/CarcassonneApp.scala
@@ -9,7 +9,10 @@ object CarcassonneApp extends JFXApp3:
     val controller = new GameMapController(model, view)
     controller.initialize()
 
+
     stage = new JFXApp3.PrimaryStage:
       title = "Carcassonne Map"
       scene = new Scene(600, 600):
+        stylesheets.add(getClass.getResource("placeholderTile.css").toExternalForm)
+        println(stylesheets)
         root = view
--- src/main/scala/GameMapView.scala
+++ src/main/scala/GameMapView.scala
@@ -1,10 +1,19 @@
 
 import scalafx.application.JFXApp3
 import scalafx.scene.Scene
-import scalafx.scene.layout.GridPane
+import scalafx.scene.layout.{Border, BorderStroke, GridPane, Region}
 import scalafx.scene.control.Button
 import scalafx.geometry.Insets
-import scalafx.Includes._
+import scalafx.Includes.*
+import scalafx.scene.paint.CycleMethod.NoCycle
+import scalafx.scene.paint.{Color, LinearGradient, Stops}
+import scalafx.scene.shape.Rectangle
+import scalafx.scene.Cursor.Hand
+import scalafx.scene.paint.Color.{Black, Grey}
+import scalafx.scene.layout.BorderStrokeStyle.Dashed
+import scalafx.scene.layout.CornerRadii.Empty
+import scalafx.scene.layout.BorderWidths.Default
+
 class GameMapView extends GridPane {
   private val mapSize = 5 // 5x5 grid for simplicity
   private var _lastPosition = Position(0, 0)
@@ -12,17 +21,17 @@ class GameMapView extends GridPane {
   // Create a 5x5 grid of buttons
   for x <- 0 until mapSize do
     for y <- 0 until mapSize do
-      val btn = new Button {
-        text = s"($x, $y)"
+      val placeholderTile = new Region {
         prefWidth = 100
         prefHeight = 100
+        styleClass += "placeholderTile"
       }
+      add(placeholderTile, x, y) // Add the button to the grid
 
-      add(btn, x, y) // Add the button to the grid
-      // Button action when clicked
-      btn.onAction = _ => notifyTileClick(Position(x, y))
+      // Region action when clicked
+      placeholderTile.onMouseClicked = _ => notifyTileClick(Position(x, y))
 
-  def lastTilePlacedPosition = _lastPosition
+  def lastTilePlacedPosition: Position = _lastPosition
   def notifyTileClick(position: Position): Unit =
   // Placeholder for controller to handle tile placement
     log(s"Tile placed at $position")
