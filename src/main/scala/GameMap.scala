98aacdb75b484cf04f319a83255479575c5baa4e
Gian Luca Nediani <gianluca.nediani@studio.unibo.it>
2024-08-19 12:38:03 +0200

    feat: implement logic for tile placement wrt other tiles in the maps

--- src/main/scala/GameMap.scala
+++ src/main/scala/GameMap.scala
@@ -6,15 +6,30 @@ class GameMap extends Subject[GameMap]:
   def placeTile(tile: GameTile, position: Position): Unit =
     if tiles.contains(position) then
       throw IllegalArgumentException(s"Tile already placed at position $position")
-    else
+
+    if isValidPlacement(tile, position) then
       tiles = tiles + (position -> tile)
       log("Tile placed")
       notifyObservers()
+    else
+      throw IllegalArgumentException(s"Invalid tile placement at position $position")
 
   def getTile(position: Position): Option[GameTile] =
     tiles.get(position)
 
-  def getTileMap(): Option[Map[Position, GameTile]] = Option.apply(this.tiles)
+  def getTileMap: Option[Map[Position, GameTile]] = Option.apply(this.tiles)
 
   def log(string: String): Unit =
     print(s"MODEL - " + string + "\n")
+
+  private def isValidPlacement(tile: GameTile, position: Position): Boolean =
+    val neighbors = List(
+      (Position(position.x, position.y - 1), tile.north, (gt: GameTile) => gt.south), // North neighbor
+      (Position(position.x + 1, position.y), tile.east, (gt: GameTile) => gt.west), // East neighbor
+      (Position(position.x, position.y + 1), tile.south, (gt: GameTile) => gt.north), // South neighbor
+      (Position(position.x - 1, position.y), tile.west, (gt: GameTile) => gt.east) // West neighbor
+    )
+
+    neighbors.forall { case (pos, tileEdge, getNeighborEdge) =>
+      tiles.get(pos).forall(neighborTile => getNeighborEdge(neighborTile) == tileEdge)
+    }
--- src/test/scala/GameMapSuite.scala
+++ src/test/scala/GameMapSuite.scala
@@ -23,3 +23,86 @@ class GameMapSuite extends AnyFunSuite with Matchers:
 
     an [IllegalArgumentException] should be thrownBy map.placeTile(tile2, position)
   }
+
+  test("Valid placement should return true for a matching neighbor tile") {
+    val map = GameMap()
+    val tile1 = GameTile(EdgeType.City, EdgeType.Road, EdgeType.Field, EdgeType.City)
+    val position1 = Position(0, 0)
+    map.placeTile(tile1, position1)
+
+    val tile2 = GameTile(EdgeType.Field, EdgeType.City, EdgeType.Road, EdgeType.Field)
+    val position2 = Position(0, 1) // South of position1
+
+    noException should be thrownBy map.placeTile(tile2, position2)
+  }
+
+  test("Valid placement with multiple matching neighbors") {
+    val map = GameMap()
+
+    val tile1 = GameTile(EdgeType.City, EdgeType.Road, EdgeType.Field, EdgeType.City)
+    val tile2 = GameTile(EdgeType.Field, EdgeType.Field, EdgeType.City, EdgeType.Road)
+    val tile3 = GameTile(EdgeType.Field, EdgeType.City, EdgeType.Road, EdgeType.Road)
+
+    map.placeTile(tile1, Position(0, 0))
+    map.placeTile(tile2, Position(0, 1)) // South
+    map.placeTile(tile3, Position(1, 0)) // East
+
+    val tile4 = GameTile(EdgeType.Road, EdgeType.City, EdgeType.City, EdgeType.Field)
+    val position4 = Position(1, 1) // Southeast corner, connects to all three
+
+    noException should be thrownBy map.placeTile(tile4, position4)
+  }
+
+  test("Invalid placement with a mismatching edge") {
+    val map = GameMap()
+
+    val tile1 = GameTile(EdgeType.City, EdgeType.Road, EdgeType.Field, EdgeType.City)
+    val position1 = Position(0, 0)
+    map.placeTile(tile1, position1)
+
+    val invalidTile = GameTile(EdgeType.Road, EdgeType.City, EdgeType.City, EdgeType.Field)
+    val invalidPosition = Position(0, 1) // South of position1, but does not match
+
+    an [IllegalArgumentException] should be thrownBy map.placeTile(invalidTile, invalidPosition)
+  }
+
+  test("Valid placement with rotation") {
+    val map = GameMap()
+
+    val tile1 = GameTile(EdgeType.City, EdgeType.Road, EdgeType.Field, EdgeType.City)
+    val position1 = Position(0, 0)
+    map.placeTile(tile1, position1)
+
+    val tile2 = GameTile(EdgeType.Road, EdgeType.City, EdgeType.Field, EdgeType.Road).rotate.rotate
+    val position2 = Position(0, 1) // South of position1, requires double rotation to match
+
+    noException should be thrownBy map.placeTile(tile2, position2)
+  }
+
+  test("Valid placement in an open space surrounded by tiles") {
+    val map = GameMap()
+
+    val tile1 = GameTile(EdgeType.City, EdgeType.Road, EdgeType.Road, EdgeType.City)
+    val tile2 = GameTile(EdgeType.Road, EdgeType.Field, EdgeType.City, EdgeType.Road)
+    val tile3 = GameTile(EdgeType.Field, EdgeType.City, EdgeType.Road, EdgeType.Road)
+    val tile4 = GameTile(EdgeType.City, EdgeType.City, EdgeType.Road, EdgeType.Field)
+
+    map.placeTile(tile1, Position(0, 0))
+    map.placeTile(tile2, Position(0, 1)) // South
+    map.placeTile(tile3, Position(1, 0)) // East
+    map.placeTile(tile4, Position(-1, 0)) // West
+
+    val northTile = GameTile(EdgeType.City, EdgeType.Road, EdgeType.City, EdgeType.City)
+    val northPosition = Position(0, -1)
+
+    noException should be thrownBy map.placeTile(northTile, northPosition)
+  }
+
+  test("Valid placement with no neighbors (first tile)") {
+    val map = GameMap()
+    val tile = GameTile(EdgeType.City, EdgeType.Road, EdgeType.Field, EdgeType.City)
+    val position = Position(0, 0)
+
+    noException should be thrownBy map.placeTile(tile, position)
+    map.getTile(position) shouldBe Some(tile)
+  }
