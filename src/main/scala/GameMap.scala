e1d26f55fb59e72fbb3fe802ae90df7515bcf699
Gian Luca Nediani <gianluca.nediani@studio.unibo.it>
2024-08-19 16:22:11 +0200

    doc: add scaladoc

--- src/main/scala/GameMap.scala
+++ src/main/scala/GameMap.scala
@@ -1,8 +1,26 @@
+/**
+ * Represents a position on the game map.
+ *
+ * @param x The x-coordinate of the position.
+ * @param y The y-coordinate of the position.
+ */
 case class Position(x: Int, y: Int)
 
+/**
+ * Represents the game map, which holds the placed tiles.
+ *
+ * This class extends `Subject[GameMap]`, meaning it can notify observers of changes.
+ */
 class GameMap extends Subject[GameMap]:
   private var tiles: Map[Position, GameTile] = Map()
 
+  /**
+   * Places a tile on the map at the specified position.
+   *
+   * @param tile The `GameTile` to place.
+   * @param position The `Position` where the tile should be placed.
+   * @throws IllegalArgumentException if a tile is already placed at the position or if the placement is invalid.
+   */
   def placeTile(tile: GameTile, position: Position): Unit =
     if tiles.contains(position) then
       throw IllegalArgumentException(s"Tile already placed at position $position")
@@ -14,14 +32,37 @@ class GameMap extends Subject[GameMap]:
     else
       throw IllegalArgumentException(s"Invalid tile placement at position $position")
 
+  /**
+   * Retrieves the tile at the specified position, if any.
+   *
+   * @param position The `Position` to retrieve the tile from.
+   * @return An `Option` containing the `GameTile` if one exists at the position, or `None` otherwise.
+   */
   def getTile(position: Position): Option[GameTile] =
     tiles.get(position)
 
+  /**
+   * Retrieves the entire map of placed tiles.
+   *
+   * @return An `Option` containing a `Map` of `Position` to `GameTile`.
+   */
   def getTileMap: Option[Map[Position, GameTile]] = Option.apply(this.tiles)
 
+  /**
+   * Logs a message with a specific format.
+   *
+   * @param string The message to log.
+   */
   def log(string: String): Unit =
     print(s"MODEL - " + string + "\n")
 
+  /**
+   * Validates whether a tile can be placed at the specified position.
+   *
+   * @param tile The `GameTile` to validate.
+   * @param position The `Position` where the tile is to be placed.
+   * @return `true` if the placement is valid, `false` otherwise.
+   */
   private def isValidPlacement(tile: GameTile, position: Position): Boolean =
     val neighbors = List(
       (Position(position.x, position.y - 1), tile.north, (gt: GameTile) => gt.south), // North neighbor
