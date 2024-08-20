18be9dd43b9b598e5cdc5acb05a310870faa43f4
Gian Luca Nediani <gianluca.nediani@studio.unibo.it>
2024-08-20 12:49:29 +0200

    feat: implement tile deck functionalities along with test suite

---
+++ src/test/scala/TileDeckSuite.scala
@@ -0,0 +1,31 @@
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers
+import scala.util.Try
+
+class TileDeckSuite extends AnyFunSuite with Matchers {
+
+  test("TileDeck should initialize with tiles from the default config file") {
+    val deck = new TileDeck()
+    deck.isEmpty shouldBe false
+  }
+
+  test("TileDeck should allow drawing a tile and reduce the number of tiles") {
+    val deck = new TileDeck()
+    val initialSize = Try(deck.draw()).map(_ => deck.getSize).getOrElse(0)
+    val tileOpt = deck.draw()
+
+    tileOpt should not be None
+    deck.getSize shouldBe initialSize - 1
+  }
+
+  test("TileDeck should return None when drawing from an empty deck") {
+    val deck = new TileDeck()
+
+    // Draw all tiles
+    while (deck.draw().isDefined) {}
+
+    deck.isEmpty shouldBe true
+    deck.draw() shouldBe None
+  }
+  
+}
