70e071319f86100696dc58c9d38db2ec0226e5d9
FPirazz <federicopirazzoli99@gmail.com>
2024-08-20 18:01:27 +0200

    doc: Added documentation also for the test classes

--- src/test/scala/GameMapControllerSuite.scala
+++ src/test/scala/GameMapControllerSuite.scala
@@ -2,8 +2,15 @@ import mainApplication.{GameMap, GameMapController, GameMapView, Position}
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers.shouldBe
 
+/**
+ * Test suite for the `GameMapController` class.
+ * This suite contains unit tests to verify the behavior of the `GameMapController`.
+ */
 class GameMapControllerSuite extends AnyFunSuite:
 
+  /**
+   * Test to verify that the controller is added as an observer to the view upon initialization.
+   */
   test("initialize should add the controller as an observer to the view") {
     val model = GameMap()
     val view = GameMapView()
@@ -13,6 +20,9 @@ class GameMapControllerSuite extends AnyFunSuite:
     view.getObservers.contains(controller) shouldBe true
   }
 
+  /**
+   * Test to verify that the `placeTile` method places a tile in the model at the specified position.
+   */
   test("placeTile should place a tile in the model") {
     val model = GameMap()
     val view = GameMapView()
@@ -23,6 +33,9 @@ class GameMapControllerSuite extends AnyFunSuite:
     model.getTileMap.get.contains(position) shouldBe true
   }
 
+  /**
+   * Test to verify that the `receiveTilePlacementAttempt` method calls `placeTile` with the correct position.
+   */
   test("receiveTilePlacementAttempt should call placeTile with the correct position") {
     val model = GameMap()
     val view = GameMapView()
--- src/test/scala/GameMapViewSuite.scala
+++ src/test/scala/GameMapViewSuite.scala
@@ -7,9 +7,15 @@ import scalafx.scene.layout.{GridPane, Region}
 import scalafx.Includes.jfxNode2sfx
 import scalafx.scene.SceneIncludes.jfxNode2sfx
 
-
+/**
+ * Test suite for the `GameMapView` class.
+ * This suite contains unit tests to verify the behavior of the `GameMapView`.
+ */
 class GameMapViewSuite extends AnyFunSuite:
 
+  /**
+   * Test to verify that the `createPlaceholderTile` method creates a tile with the correct properties.
+   */
   test("createPlaceholderTile should create a tile with correct properties") {
     val view = GameMapView()
     val position = Position(1, 1)
@@ -20,6 +26,9 @@ class GameMapViewSuite extends AnyFunSuite:
     placeholderTile.styleClass.contains("placeholderTile") shouldBe true
   }
 
+  /**
+   * Test to verify that the `placeTile` method places a tile at the correct position in the view.
+   */
   test("placeTile should place a tile at the correct position") {
     val view = GameMapView()
     val position = Position(1, 1)
@@ -31,6 +40,9 @@ class GameMapViewSuite extends AnyFunSuite:
     view.getChildren.contains(placedTile) shouldBe true
   }
 
+  /**
+   * Test to verify that the `createNewPlaceholders` method creates new placeholders around the last placed tile.
+   */
   test("createNewPlaceholders should create new placeholders around the last placed tile") {
     val view = GameMapView()
     val position = Position(2, 2)
@@ -53,6 +65,9 @@ class GameMapViewSuite extends AnyFunSuite:
     )
   }
 
+  /**
+   * Test to verify that the `checkClickedTile` method notifies observers of a tile placement attempt.
+   */
   test("checkClickedTile should notify observers of tile placement attempt") {
     val view = GameMapView()
     val position = Position(1, 1)
