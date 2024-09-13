6b939ff54dd9f48686ce9ae7a9133d04f4e6d1db
FPirazz <federicopirazzoli99@gmail.com>
2024-09-13 18:12:57 +0200

    refactor: Completely refactored GameMatchFollowerGridView class

--- src/main/scala/carcassonne/view/gameMatch/GameMatchFollowerGridView.scala
+++ src/main/scala/carcassonne/view/gameMatch/GameMatchFollowerGridView.scala
@@ -9,96 +9,50 @@ import scalafx.scene.layout.{ColumnConstraints, GridPane, RowConstraints, StackP
 import carcassonne.util.{Logger, Position}
 import scalafx.Includes.*
 
-class GameMatchFollowerGridView(availSegments: List[TileSegment],
-                                drawnTileImage: ImageView,
-                                currentPlayer: Player,
-                                position: Position,
-                               notifyFollowerPlacement: (Position, TileSegment, Player) => Unit)
-  extends StackPane:
+class GameMatchFollowerGridView(
+                                 availSegments: List[TileSegment],
+                                 drawnTileImage: ImageView,
+                                 currentPlayer: Player,
+                                 position: Position,
+                                 notifyFollowerPlacement: (Position, TileSegment, Player) => Unit
+                               ) extends StackPane:
 
-  // Replace the tile that has been just removed with new attributes
-  val followerGrid = new GridPane():
+  private val followerGrid = new GridPane:
     hgap = 0
     vgap = 0
     padding = Insets(0)
     alignment = Pos.Center
+    prefWidth = drawnTileImage.fitHeight.toDouble
+    prefHeight = drawnTileImage.fitHeight.toDouble
 
-    // Creating 3x3 grid structure with equal cell sizes
-    columnConstraints ++= Seq(
-      new ColumnConstraints {
-        percentWidth = 100 / 3.0
-      },
-      new ColumnConstraints {
-        percentWidth = 100 / 3.0
-      },
-      new ColumnConstraints {
-        percentWidth = 100 / 3.0
-      }
-    )
+    columnConstraints ++= Seq.fill(3)(new ColumnConstraints { percentWidth = 100 / 3.0 })
+    rowConstraints ++= Seq.fill(3)(new RowConstraints { percentHeight = 100 / 3.0 })
 
-    rowConstraints ++= Seq(
-      new RowConstraints {
-        percentHeight = 100 / 3.0
-      },
-      new RowConstraints {
-        percentHeight = 100 / 3.0
-      },
-      new RowConstraints {
-        percentHeight = 100 / 3.0
-      }
-    )
-
-  followerGrid.prefHeight <== drawnTileImage.fitHeight.toDouble
-  followerGrid.prefWidth <== drawnTileImage.fitWidth.toDouble
-
-  availSegments.foreach(segment =>
-    val followerImageView = new ImageView(new Image(getClass.getResource("../../../follower.png").toExternalForm)):
+  availSegments.foreach { segment =>
+    val followerOutline = new ImageView(new Image(getClass.getResource("../../../follower.png").toExternalForm)):
       fitWidth = (drawnTileImage.fitWidth.toDouble - 5) / 3.3
       fitHeight = (drawnTileImage.fitHeight.toDouble - 5) / 3.3
       preserveRatio = true
 
-    // Create the overlay meeple with the desired fill color
     val filledFollower = new ImageView(new Image(getClass.getResource("../../../follower_filled.png").toExternalForm)):
       fitWidth = (drawnTileImage.fitWidth.toDouble - 5) / 3.3
       fitHeight = (drawnTileImage.fitHeight.toDouble - 5) / 3.3
-      opacity = 0.5 // Set the opacity to make it semi-transparent
-      visible = true // Initially not visible
+      opacity = 0.5
+      visible = true
       preserveRatio = true
+      onMouseEntered = _ => this.effect = currentPlayer.getPlayerColor
+      onMouseExited =  _ => this.effect = null
 
-
-    // Create a StackPane to hold the ImageView and Rectangle
-    val stackPane = new StackPane {
-      children = Seq(followerImageView, filledFollower)
-    }
-
-    // Add hover effect to show/hide the rectangle
-    filledFollower.onMouseEntered = _ =>
-      filledFollower.effect = currentPlayer.getPlayerColor
-
-    filledFollower.onMouseExited = _ =>
-      filledFollower.effect = null
-
-    var x = 1
-    var y = 1
-
-    segment match
-      case TileSegment.N => y -= 1
-      case TileSegment.E => x += 1
-      case TileSegment.S => y += 1
-      case TileSegment.W => x -= 1
-      case TileSegment.NE =>
-        x += 1
-        y -= 1
-      case TileSegment.NW =>
-        x -= 1
-        y -= 1
-      case TileSegment.SE =>
-        x += 1
-        y += 1
-      case TileSegment.SW =>
-        x -= 1
-        y += 1
-      case _ =>
+    val (x, y) = segment match
+      case TileSegment.N  => (1, 0)
+      case TileSegment.E  => (2, 1)
+      case TileSegment.S  => (1, 2)
+      case TileSegment.W  => (0, 1)
+      case TileSegment.NE => (2, 0)
+      case TileSegment.NW => (0, 0)
+      case TileSegment.SE => (2, 2)
+      case TileSegment.SW => (0, 2)
+      case _              => (1, 1)
 
     filledFollower.onMouseClicked = (event: MouseEvent) => if event.button == MouseButton.Primary then
       filledFollower.onMouseEntered = null
@@ -110,8 +64,11 @@ class GameMatchFollowerGridView(availSegments: List[TileSegment],
       )
       filledFollower.onMouseClicked = null
 
-    followerGrid.add(stackPane, x, y)
-  )
+    followerGrid.add(
+      new StackPane:
+        children = Seq(followerOutline, filledFollower)
+      , x, y)
+  }
 
   this.maxWidth = 10
   this.maxHeight = 10
